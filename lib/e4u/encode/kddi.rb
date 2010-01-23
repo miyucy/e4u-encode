a427dc3903c3220e4971991eacf0881a12106e04
fistfvck <fistfvck@gmail.com>
2010-01-23 11:44:53 +0900

    ネスト浅くした

--- lib/e4u/encode/kddi.rb
+++ lib/e4u/encode/kddi.rb
@@ -1,49 +1,47 @@
-module E4U
-  module Encode
-    module KDDI
-      autoload :Cp932,   'e4u/encode/kddi/cp932'
-      autoload :Utf8,    'e4u/encode/kddi/utf8'
-      autoload :Unicode, 'e4u/encode/kddi/unicode'
+module E4U::Encode
+  module KDDI
+    autoload :Cp932,   'e4u/encode/kddi/cp932'
+    autoload :Utf8,    'e4u/encode/kddi/utf8'
+    autoload :Unicode, 'e4u/encode/kddi/unicode'
 
-      def self.encode str, from, to
-        raise unless from[:carrier] == :kddi
-        if to[:carrier] == :kddi
-          encode_carrier(str, from, to)
-        else
-          encode_encoding(str, from, to)
-        end
+    def self.encode str, from, to
+      raise unless from[:carrier] == :kddi
+      if to[:carrier] == :kddi
+        encode_carrier(str, from, to)
+      else
+        encode_encoding(str, from, to)
       end
+    end
 
-      private
+    private
 
-      def self.encode_carrier str, from, to
-        return encode_encoding(str, from, to) if from[:encoding] != :unicode
-        case to[:encoding]
-        when :sjis
-          from[:encoding] = :sjis
-          Unicode.to_cp932(Encoding.to_cp932(str))
-        when :utf8
-          from[:encoding] = :utf8
-          Unicode.to_utf8(str)
-        else
-          raise
-        end
+    def self.encode_carrier str, from, to
+      return encode_encoding(str, from, to) if from[:encoding] != :unicode
+      case to[:encoding]
+      when :sjis
+        from[:encoding] = :sjis
+        Unicode.to_cp932(Encoding.to_cp932(str))
+      when :utf8
+        from[:encoding] = :utf8
+        Unicode.to_utf8(str)
+      else
+        raise
       end
+    end
 
-      def self.encode_encoding str, from, to
-        case from[:encoding]
-        when :sjis
-          from[:encoding] = :unicode
-          Encoding.to_utf8(Cp932.to_kddi_unicode(str))
-        when :utf8
-          from[:encoding] = :unicode
-          Utf8.to_kddi_unicode(str)
-        when :unicode
-          from[:carrier] = :google
-          Unicode.to_google_unicode(str)
-        else
-          raise
-        end
+    def self.encode_encoding str, from, to
+      case from[:encoding]
+      when :sjis
+        from[:encoding] = :unicode
+        Encoding.to_utf8(Cp932.to_kddi_unicode(str))
+      when :utf8
+        from[:encoding] = :unicode
+        Utf8.to_kddi_unicode(str)
+      when :unicode
+        from[:carrier] = :google
+        Unicode.to_google_unicode(str)
+      else
+        raise
       end
     end
   end
