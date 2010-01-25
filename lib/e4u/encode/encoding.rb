b9bd1f3cc9d9d69db4971c50ccbd59ed25ab2463
fistfvck <fistfvck@gmail.com>
2010-01-25 19:00:53 +0900

    nkfオプションを統一した

--- lib/e4u/encode/encoding.rb
+++ lib/e4u/encode/encoding.rb
@@ -6,7 +6,7 @@ module E4U
       def self.to_utf8 str
         puts "cp932_to_utf8" if $DEBUG
         if RUBY_VERSION < '1.9.1'
-          NKF.nkf('-m0wS --ic=CP932', str)
+          NKF.nkf('-m0Sw --ic=CP932 --oc=UTF-8', str)
         else
           str.encode('UTF-8')
         end
@@ -15,7 +15,7 @@ module E4U
       def self.to_cp932 str
         puts "utf8_to_cp932" if $DEBUG
         if RUBY_VERSION < '1.9.1'
-          NKF.nkf('-m0Ws --oc=CP932', str)
+          NKF.nkf('-m0sW --ic=UTF-8 --oc=CP932', str)
         else
           str.encode('CP932')
         end
--- spec/encoding_spec.rb
+++ spec/encoding_spec.rb
@@ -6,8 +6,8 @@ describe E4U::Encode::Encoding do
     if RUBY_VERSION < '1.9.1'
       it "SJIS文字列がUTF-8に変換されること" do
         utf8 = "とある文字列の符号化方式＜エンコーディング＞"
-        sjis = NKF.nkf('-Wsm0x', utf8)
-        NKF.should_receive(:nkf).once.with('-m0wS --ic=CP932', sjis).and_return(utf8)
+        sjis = NKF.nkf('-m0sW --ic=UTF-8 --oc=CP932', utf8)
+        NKF.should_receive(:nkf).once.with('-m0Sw --ic=CP932 --oc=UTF-8', sjis).and_return(utf8)
         E4U::Encode::Encoding.to_utf8(sjis)
       end
     else
@@ -31,8 +31,8 @@ describe E4U::Encode::Encoding do
     if RUBY_VERSION < '1.9.1'
       it "UTF-8文字列がSJISに変換されること" do
         utf8 = "とある文字列の符号化方式＜エンコーディング＞"
-        sjis = NKF.nkf('-Wsm0x', utf8)
-        NKF.should_receive(:nkf).once.with('-m0Ws --oc=CP932', utf8).and_return(sjis)
+        sjis = NKF.nkf('-m0Sw --ic=CP932 --oc=UTF-8', utf8)
+        NKF.should_receive(:nkf).once.with('-m0sW --ic=UTF-8 --oc=CP932', utf8).and_return(sjis)
         E4U::Encode::Encoding.to_cp932(utf8)
       end
     else
